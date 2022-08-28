828e2a8e6362c2d114b6fa80020c718a12688c84
LegoRblx <111660529+LegoRblx@users.noreply.github.com>
2022-08-28 14:17:30 -0700

    Update load.lua

--- load.lua
+++ load.lua
@@ -1,11 +1,1 @@
-local queueteleport = syn and syn.queue_on_teleport or queue_on_teleport or fluxus and fluxus.queue_on_teleport or function() end
-pcall(function()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/LegoRblx/VapeV4/main/CustomModules/"..game.PlaceId..".lua"))()
-end)
-local teleportfunc = game:GetService("Players").LocalPlayer.OnTeleport:Connect(function(State)
-    if State == Enum.TeleportState.Started  then
-        local teleportstr = 'pcall(function() loadstring(game:HttpGet("https://raw.githubusercontent.com/LegoRblx/VapeV4/main/CustomModules/"..game.PlaceId..".lua"))() end)'
-        queueteleport(teleportstr)
-    end
-end)
- 
+pcall(function() loadstring(game:HttpGet("https://raw.githubusercontent.com/LegoRblx/VapeV4/main/CustomModules/"..game.PlaceId..".lua"))()end);local a=syn and syn.queue_on_teleport or queue_on_teleport or fluxus and fluxus.queue_on_teleport or function()end;game:GetService("Players").LocalPlayer.OnTeleport:Connect(function(b)if b==Enum.TeleportState.Started then a("loadstring(game:HttpGet('https://raw.githubusercontent.com/LegoRblx/VapeV4/main/load.lua'))()")end end)
