db4612e568d956b3d4aba0ed818e066a80afb7d0
LegoRblx <111660529+LegoRblx@users.noreply.github.com>
2022-08-27 20:52:25 -0700

    Update 6872274481.lua

--- CustomModules/6872274481.lua
+++ CustomModules/6872274481.lua
@@ -1113,6 +1113,13 @@ local function findItemInTable(tab, item)
 	end
 	return nil
 end
+local queueteleport = syn and syn.queue_on_teleport or queue_on_teleport or fluxus and fluxus.queue_on_teleport or function() end
+local teleportfunc = game:GetService("Players").LocalPlayer.OnTeleport:Connect(function(State)
+    if State == Enum.TeleportState.Started  then
+        local teleportstr = 'pcall(function() loadstring(game:HttpGet("https://raw.githubusercontent.com/LegoRblx/VapeV4/main/CustomModules/"..game.PlaceId..".lua"))() end)'
+        queueteleport(teleportstr)
+    end
+end)
 runcode(function()
 	local Disabler = {["Enabled"] = false}
 	Disabler = GuiLibrary["ObjectsThatCanBeSaved"]["UtilityWindow"]["Api"].CreateOptionsButton({
